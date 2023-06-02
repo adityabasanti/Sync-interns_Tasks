bfd75288d6ea91e5bea14b795593a795f624d0c9
Aditya <aditya@dev.com>
2023-06-02 12:16:43 +0530

    task 3

---
+++ TASK 3- Music Web App/data.js
@@ -0,0 +1,33 @@
+let songs=[
+    {
+        name:'On_on',
+        path:'Music/Song 1.mp3',
+        artist:'ft. Daniel levi',
+        cover:'Images/song 1.jpg'
+    },
+    {
+        name:'Symbolism',
+        path:'Music/Song 2.mp3',
+        artist:'Electro Light',
+        cover:'Images/song 2.jpg'
+    },
+    {
+        name:"I'm good",
+        path:'Music/Song 3.mp3',
+        artist:'Nightcore',
+        cover:'Images/song 3.jpg'
+    },
+    {
+        name:'Dreams',
+        path:'Music/Song 4.mp3',
+        artist:'Lost sky',
+        cover:'Images/song 4.jpg'
+    },
+    {
+        name:'Mashup',
+        path:'Music/Song 5.mp3',
+        artist:'Nightcore',
+        cover:'Images/song 5.jpg'
+    },
+    
+]
